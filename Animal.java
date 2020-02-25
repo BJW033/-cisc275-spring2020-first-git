305903a27e11c69c748af032299a6b49b416bb82
Brandon Wu <brandonjameswu1@gmail.com>
2020-02-25 16:04:02 -0500

    Added Author and changed the order of parameters in constructor to match Dog

--- Animal.java
+++ Animal.java
@@ -1,7 +1,8 @@
+//Brandon Wu
 public class Animal{
 	int legs;
 	String name;
-	public Animal(int legs, String name){
+	public Animal(String name, int legs){
 		this.legs = legs;
 		this.name = name;
 	}
